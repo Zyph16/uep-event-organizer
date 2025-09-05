70b549684943796d6ceec097a90b857f3a41c42b
KennSabelao <kennsabilao@gmail.com>
2025-09-05 09:12:29 +0800

    Update project files and add images folder

--- index.php
+++ index.php
@@ -12,16 +12,20 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto:ital,wght@0,100..900;1,100..900&family=Schibsted+Grotesk:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24..48,100..700,0..1,-25..200" rel="stylesheet" />
-
+    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.css" rel="stylesheet">
 
 
 </head>
 <body>
     <?php include("components/header/header.php")?>
-    <?php include("components/hero/hero.html")?>
-    <?php include("components/about-us/about-us.html")?>
-    <?php include("components/facilities/facilities.php")?>
+    <?php include("app/LandingPage.php")?>
+
+
+
     <script src="js/header.js"></script>
     <script src="js/facilities.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
+    <script src="js/calendar.js"></script>
+
 </body>
 </html>
